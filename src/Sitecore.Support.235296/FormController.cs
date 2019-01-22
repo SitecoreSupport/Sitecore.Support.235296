3db2d1a2094e7f9ea8f848c1d372bb04efe1561b
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2019-01-22 14:43:35 +0200

    Prevent attempts to bypass the reCaptcha field validation

---
+++ src/Sitecore.Support.235296/FormController.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Forms.Mvc.Controllers.ModelBinders;
+using Sitecore.Forms.Mvc.ViewModels;
+using System.Web.Mvc;
+using Sitecore.Support.Forms.Mvc.Validators;
+
+namespace Sitecore.Support.Forms.Mvc.Controllers
+{
+  public class FormController : Sitecore.Forms.Mvc.Controllers.FormController
+  {
+    [ReCaptchaValidator(ParamName = "formViewModel")]
+    public override ActionResult Index([ModelBinder(typeof(FormModelBinder))] FormViewModel formViewModel)
+    {
+      return base.Index(formViewModel);
+    }
+  }
+}
