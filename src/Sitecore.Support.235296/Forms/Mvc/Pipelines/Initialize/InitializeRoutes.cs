d9ac7aeabe9695b78455768568181221c1339a88
Chee How Yap <yho@sitecore.net>
2019-10-26 13:13:57 +0800

    Commit Patch

---
+++ src/Sitecore.Support.235296/Forms/Mvc/Pipelines/Initialize/InitializeRoutes.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Pipelines;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Sitecore.Support.Forms.Mvc.Pipelines.Initialize
+{
+  public class InitializeRoutes : Sitecore.Mvc.Pipelines.Loader.InitializeRoutes
+  {
+    protected override void RegisterRoutes(RouteCollection routes, PipelineArgs args)
+    {
+      if (routes.Remove(RouteTable.Routes["Form"]))
+      {
+        routes.MapRoute("Form", "form/{action}", new
+        {
+          controller = "Form",
+          action = "Process"
+        }, new string[1]
+        {
+          "Sitecore.Support.Forms.Mvc.Controllers"
+        });
+      }
+    }
+  }
+}
